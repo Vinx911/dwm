1e5c939a7e037573232c492bce407b47bcfa2c90
Vinx911 <BuddyHe911@163.com>
2022-10-03 18:19:17 +0800

    patch winicon

--- config.def.h
+++ config.def.h
@@ -1,5 +1,8 @@
 /* See LICENSE file for copyright and license details. */
 
+#define ICONSIZE 16 /* icon size */
+#define ICONSPACING 5 /* space between icon and title */
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
