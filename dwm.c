174ca8ccce71ccb2114efb8926e1ed097b33b644
Vinx911 <BuddyHe911@163.com>
2022-10-04 21:01:43 +0800

    monocle布局添加间距

--- dwm.c
+++ dwm.c
@@ -1832,6 +1832,8 @@ void maprequest(XEvent* e)
 void monocle(Monitor* m)
 {
     unsigned int n = 0;
+    unsigned int gap = m->gappx;
+
     Client* c;
 
     for (c = m->clients; c; c = c->next) {
@@ -1843,7 +1845,7 @@ void monocle(Monitor* m)
         snprintf(m->ltsymbol, sizeof m->ltsymbol, "[%d]", n);
     }
     for (c = nexttiled(m->clients); c; c = nexttiled(c->next)) {
-        resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw, 0);
+        resize(c, m->wx + gap, m->wy + gap, m->ww - 2 * c->bw - 2 * gap, m->wh - 2 * c->bw - 2 * gap, 0);
     }
 }
 
